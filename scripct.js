86c75815a6f8577cd079083a3a20f8ac0d47cd12
hungdong <hunghung@gmail.com>
2024-10-31 20:58:00 +0700

    add all files

---
+++ scripct.js
@@ -0,0 +1,27 @@
+/* vuốt chuyển slide */
+let touchstartX = 0;
+let touchendX = 0;
+
+const carousel = document.getElementById('carouselExampleIndicators');
+
+carousel.addEventListener('touchstart', e => {
+    touchstartX = e.changedTouches[0].screenX;
+});
+
+carousel.addEventListener('touchend', e => {
+    touchendX = e.changedTouches[0].screenX;
+    handleGesture();
+});
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        var carouselInstance = new bootstrap.Carousel(carousel);
+        carouselInstance.next();
+    }
+
+    if (touchendX > touchstartX) {
+        var carouselInstance = new bootstrap.Carousel(carousel);
+        carouselInstance.prev();
+    }
+}
+/* vuốt chuyển slide end */
